ae8e83cda7a48a9e4b4c21a3e969cbd7ab2cb654
abdelkayoumk <abdelkayoum88@gmail.com>
2025-10-18 11:07:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def b(t):
   if t%10 in [0,1,8,9]:
     return 0
@@ -29,7 +27,7 @@ for _ in range(FN):
   flows[f] = [x, y, t_start, Q_total, m1, n1, m2, n2]
 
 for t in range(T):
-  checked = np.zeros((M, N))
+  checked = [[0 for _ in range(M)] for _ in range(N)]
   for f in flows:
     if flows[f][2] <= t and flows[f][3] > 0 and b(t + UAV[(flows[f][0] , flows[f][1])][1])> 0 :
       q_transferrable = min(flows[f][3], b(t + UAV[(flows[f][0] , flows[f][1])][1]) * UAV[(flows[f][0] , flows[f][1])][0])
@@ -37,8 +35,8 @@ for t in range(T):
       m1, n1, m2, n2 = flows[f][4], flows[f][5], flows[f][6], flows[f][7]
       for i in range(m1, m2+1):
         for j in range(n1, n2+1):
-          if checked[i,j] == 0 and not found:
-            checked[i,j] = 1
+          if checked[i][j] == 0 and not found:
+            checked[i][j] = 1
             perform_flow_f(f, t, q_transferrable, i, j)
             found = True
 
