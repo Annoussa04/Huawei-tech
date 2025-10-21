922a8fbe9041886c8eb9a53e12fd9b1b32fdcce3
abdelkayoumk <abdelkayoum88@gmail.com>
2025-10-21 14:53:08 +0200

    Create test_flow_ordering.py

---
+++ test_flow_ordering.py
@@ -0,0 +1,60 @@
+def get_bandwidth_multiplier(t_effective):
+    t_mod_10 = t_effective % 10
+    if t_mod_10 in [0, 1, 8, 9]:
+        return 0.0
+    elif t_mod_10 in [2, 7]:
+        return 0.5
+    else:
+        return 1.0
+
+
+M, N, FN, T = map(int, input().split())
+
+UAVs = {}
+for _ in range(M * N):
+    line = input().split()
+    x, y, B, phi = int(line[0]), int(line[1]), float(line[2]), int(line[3])
+    UAVs[(x, y)] = {'B': B, 'phi': phi}
+
+flows = {}
+record_of_flows = {}
+for _ in range(FN):
+    f, x, y, t_start, Q_total, m1, n1, m2, n2 = map(int, input().split())
+    record_of_flows[f] = []
+    flows[f] = {
+        'access_x': x, 'access_y': y, 't_start': t_start,
+        'Q_rem': float(Q_total),
+        'm1': m1, 'n1': n1, 'm2': m2, 'n2': n2,
+        'last_uav': None, 'change_count': 0
+    }
+
+Q_total = 0
+for f in flows:
+    Q_total += flows[f]['Q_rem']
+
+
+attributed_flow = {}
+for t in range(T):
+    attributed_flow[t] = [[ (UAVs[(i,j)] ['B'] * get_bandwidth_multiplier( t + UAVs[(i,j)]['phi'])) for j in range(M)] for i in range(N)]
+
+
+sorted_flows = sorted(flows, key= lambda x : x['Q_rem'], reverse=True)
+
+
+
+
+
+for f, flow_data in sorted_flows:
+    print('')
+
+
+
+
+
+
+
+for f, records in record_of_flows.items():
+    print(f, len(records))
+    for rec in records:
+        print(f"{rec[0]} {rec[1]} {rec[2]} {rec[3]}")
+
